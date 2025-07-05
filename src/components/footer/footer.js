d8173629325b4bafcfd6b33babb330d5c970dcbb
Cherryga <155431520+Cherryga@users.noreply.github.com>
2025-07-05 11:26:54 +0530

    footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ function Footer() {
     <div className="footer">
       <h4>© This Project is made by @
       <a href="https://github.com/Cherryga/" target="blank">Cherry Garg</a>.
-      Check Out the <a href="https://github.com/AnubhavSrivastavaGithub/gh_analyzer" target="blank">Github Repo</a>.
+      Check Out the <a href="https://github.com/Cherryga/github-profile-analyzer" target="blank">Github Repo</a>.
       </h4>
     </div>
   );
